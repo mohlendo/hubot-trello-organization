99248f8194d32438e0589cdce46d837e220d0afa
Jared Barboza <jared.m.barboza@gmail.com>
2015-01-01 14:33:52 -0500

    update for manual testing

---
+++ .hubot-trello.ps1
@@ -0,0 +1,7 @@
+# sample file for setting the environment vars necessary to test hubot-trello locally
+
+$env:HUBOT_TRELLO_KEY=''
+$env:HUBOT_TRELLO_TOKEN=''
+$env:HUBOT_TRELLO_BOARD=''
+
+./bin/hubot
---
+++ .hubot-trello.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# sample file for setting the environment vars necessary to test hubot-trello locally
+
+export HUBOT_TRELLO_KEY=''
+export HUBOT_TRELLO_TOKEN=''
+export HUBOT_TRELLO_BOARD=''
+
+./bin/hubot
