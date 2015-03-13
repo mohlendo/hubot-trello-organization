bac912643e5f54c8064d33fef17f9fb1c1811cca
Manuel Ohlendorf <moh@icnh.de>
2015-03-13 17:35:44 +0100

    save board name

--- scripts/trello.js
+++ scripts/trello.js
@@ -47,24 +47,61 @@ module.exports = function (robot) {
         return room;
     }
 
+    // Updates the brain's  knowledge.
+    function updateBrain(boards) {
+        robot.brain.set('boards', boards);
+    }
+
+    // Returns all boards.
+    function getBoards() {
+        return robot.brain.get('boards') || {};
+    }
+
+    function saveBoard(room, board) {
+        var boards = getBoards();
+        boards[room] = board;
+        updateBrain(boards);
+    }
+
     function findBoard(msg, callback) {
-        var room = findRoom(msg);
+        var boardName = findRoom(msg);
+        var savedBoardName = getBoards()[room];
+        if (savedBoardName) {
+            boardName = savedBoardName;
+        }
         trello.get("/1/organizations/" + process.env.HUBOT_TRELLO_ORGANIZATION + "/boards", function (err, data) {
             if (err) {
                 callback(err);
                 return;
             }
             var boards = data.some(function (board) {
-                return board.name.toLowerCase() === room.toLowerCase();
+                return board.name.toLowerCase() === boardName.toLowerCase();
             });
             if (boards && boards.length > 0) {
                 callback(undefined, boards[0]);
             } else {
-                msg.reply("I couldn't find a board named: " + room + ".");
                 callback(undefined, undefined);
             }
         });
     }
+
+    function findList(msg, board, listName, callback) {
+        trello.get("/1/boards/" + board.id + "/lists", function (err, data) {
+            if (err) {
+                callback(err);
+                return;
+            }
+            var lists = data.some(function (list) {
+                return list.name.toLowerCase() === listName.toLowerCase();
+            });
+            if (lists && lists.length > 0) {
+                callback(undefined, lists[0]);
+            } else {
+                callback(undefined, undefined);
+            }
+        });
+    }
+
 
     // check the config first
     ensureConfig(console.log);
@@ -80,7 +117,7 @@ module.exports = function (robot) {
                 return;
             }
             data.forEach(function (board) {
-                msg.send("* " + board.id + " - " + board.name);
+                msg.send("* " + board.name);
             });
         });
     });
@@ -105,7 +142,131 @@ module.exports = function (robot) {
                         msg.send("* " + list.name);
                     });
                 });
+            } else {
+                msg.reply("I couldn't find a board named: " + room + ".");
             }
         });
+    });
+
+    // list all the cards in the given list of the current board
+    robot.respond(/list cards in [\\"\\'](.+)[\\"\\']$/i, function (msg) {
+        var room = findRoom(msg);
+        var listName = msg.match[1];
+        msg.reply("Looking up the cards for " + room + " in list" + list + ", one sec.");
+        ensureConfig(msg.send);
+        findBoard(msg, function (err, board) {
+            if (err) {
+                msg.reply("Sorry, but there was an error reading the list of boards.");
+                return;
+            }
+            if (board) {
+                findList(msg, board, listName, function (err, list) {
+                    if (err) {
+                        msg.reply("Sorry, but there was an error reading the lists.");
+                        return;
+                    }
+                    if (list) {
+                        trello.get("/1/lists/" + list.id, {cards: "open"}, function (err, data) {
+                            if (err) {
+                                msg.reply("Sorry, but there was an error showing the cards.");
+                                return;
+                            }
+                            if (data.cards.length > 0) {
+                                msg.reply("Here are all the cards in " + data.name + ":");
+                                data.cards.forEach(function (card) {
+                                    msg.send("* [" + card.shortLink + "] #" + card.name + " - " + card.shortUrl);
+                                });
+                            } else {
+                                msg.reply("No cards are currently in the " + data.name + " list.");
+                            }
+                        });
+                    } else {
+                        msg.reply("I couldn't find a list named: " + listName + ".");
+                    }
+                });
+            } else {
+                msg.reply("I couldn't find a board named: " + room + ".");
+            }
+        });
+    });
+
+    robot.respond(/create new [\\"\\'](.+)[\\"\\'] in [\\"\\'](.+)[\\"\\']$/i, function (msg) {
+        var room = findRoom(msg);
+        var cardName = msg.match[1];
+        var listName = msg.match[2];
+
+        msg.reply("Sure thing boss. I'll create that card for you.");
+        ensureConfig(msg.send);
+        findBoard(msg, function (err, board) {
+            if (err) {
+                msg.reply("Sorry, but there was an error reading the list of boards.");
+                return;
+            }
+            if (board) {
+                findList(msg, board, listName, function (err, list) {
+                    if (err) {
+                        msg.reply("Sorry, but there was an error reading the lists.");
+                        return;
+                    }
+                    if (list) {
+                        trello.post("/1/cards", {name: cardName, idList: list.id}, function (err, data) {
+                            if (err) {
+                                msg.reply("There was an error creating the card");
+                                return;
+                            }
+                            msg.reply("OK, I created that card for you. You can see it here: " + data.url);
+                        });
+                    } else {
+                        msg.reply("I couldn't find a list named: " + listName + ".");
+                    }
+                });
+            } else {
+                msg.reply("I couldn't find a board named: " + room + ".");
+            }
+        });
+    });
+
+    robot.respond(/move (\w+) to [\\"\\'](.+)[\\"\\']$/i, function (msg) {
+        var room = findRoom(msg);
+        var cardId = msg.match[1];
+        var listName = msg.match[2];
+
+        msg.reply("Sure thing boss. I'll create that card for you.");
+        ensureConfig(msg.send);
+        findBoard(msg, function (err, board) {
+            if (err) {
+                msg.reply("Sorry, but there was an error reading the list of boards.");
+                return;
+            }
+            if (board) {
+                findList(msg, board, listName, function (err, list) {
+                    if (err) {
+                        msg.reply("Sorry, but there was an error reading the lists.");
+                        return;
+                    }
+                    if (list) {
+                        trello.put("/1/cards/"+cardId+"/idList", {value: list.id}, function (err, data) {
+                            if (err) {
+                                msg.reply("Sorry boss, I couldn't move that card after all.");
+                                return;
+                            }
+                            msg.reply("Yep, ok, I moved that card to " + listName + ".");
+                        });
+                    } else {
+                        msg.reply("I couldn't find a list named: " + listName + ".");
+                    }
+                });
+            } else {
+                msg.reply("I couldn't find a board named: " + room + ".");
+            }
+        });
+    });
+
+    robot.respond(/set board to [\\"\\'](.+)[\\"\\']$/i, function (msg) {
+        ensureConfig(msg.send);
+        var room = findRoom(msg);
+        var boardName = msg.match(1);
+        saveBoard(room, boardName);
+        msg.send("Ok, from now on this room is combined to the trello board: " + boardName);
     });
 };
